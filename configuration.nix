3057d4629213fbc19ac2ea66fb8b59c5e6136ae3
kotudemo <130310308+kotudemo@users.noreply.github.com>
2025-04-25 22:22:36 +0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -253,7 +253,7 @@
 		kdePackages.kate
 	    	kdePackages.kcalc
             	kdePackages.kcolorchooser
-		inputs.freesm.freesmPackages.freesmlauncher
+		#inputs.freesm.freesmPackages.freesmlauncher
 	    	heroic
             	tetrio-desktop
 	    	spoofdpi
