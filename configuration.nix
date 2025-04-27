081a194b3990551b0a5af2096cca9a642afa45e7
kotudemo <130310308+kotudemo@users.noreply.github.com>
2025-04-27 17:51:00 +0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -253,6 +253,44 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+      wireplumber.enable = true;
+      extraConfig = {
+        pipewire = {
+                "92-low-latency" = {
+                	"context.properties" = {
+                            "default.clock.rate" = 48000;
+                            "default.clock.allowed-rates" = [
+                                44100
+                                48000
+                                88200
+                                96000
+                            ];
+
+                            "default.clock.min-quantum" = 512;
+                            "default.clock.quantum" = 4096;
+                            "default.clock.max-quantum" = 8192;
+                        };
+                    };
+
+                    "93-no-resampling" = {
+                        "context.properties" = {
+                            "default.clock.rate" = 48000;
+                            "default.clock.allowed-rates" = [
+                                44100
+                                48000
+                                96000
+                                192000
+                            ];
+                        };
+                    };
+
+                    "94-no-upmixing" = {
+                        "stream.properties" = {
+                            "channelmix.upmix" = false;
+                        };
+                    };
+                };
+            };
     };
      scx = {
       enable = true;
