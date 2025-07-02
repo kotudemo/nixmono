7fd1cf9bfd36981c0a0b0e408b691685bc0cae32
kotudemo <narutogyg@gmail.com>
2025-07-02 21:05:40 +0400

    aaa

--- flake.nix
+++ flake.nix
@@ -918,8 +918,8 @@
                 };
                 variables = {
                   HOSTNAME = config.networking.hostName;
-                  GDK_BACKEND = "wayland";
-                  QT_QPA_PLATFORM = "wayland";
+                  GDK_BACKEND = "wayland,x11";
+                  QT_QPA_PLATFORM = "wayland;xcb";
                   XDG_SESSION_TYPE = "wayland";
                   XDG_CURRENT_DESKTOP = "Hyprland";
                   XDG_SESSION_DESKTOP = "Hyprland";
