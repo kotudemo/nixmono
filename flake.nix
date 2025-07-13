e88b506359330fb3bb3fa2b215d213cf3c78c865
kotudemo <narutogyg@gmail.com>
2025-07-14 01:48:49 +0400

    fixes

--- flake.nix
+++ flake.nix
@@ -164,13 +164,10 @@
                 kernelModules = [
                   "kvm-intel"
                   "amdgpu"
-                  "v4l2loopback"
                 ];
                 extraModulePackages = with config.boot.kernelPackages; [
-                  v4l2loopback
                 ];
                 extraModprobeConfig = ''
-                  options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
                 '';
                 blacklistedKernelModules = [
                   "k10temp"
